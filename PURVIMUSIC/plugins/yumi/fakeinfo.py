a861e9076eb1afe134d51676a511bbac45dc597f
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-25 00:13:27 +0530

    Add files via upload

---
+++ PURVIMUSIC/plugins/yumi/fakeinfo.py
@@ -0,0 +1,67 @@
+"""***
+MIT License
+
+Copyright (c) [2023] [DAXX TEAM]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.***
+"""
+
+import requests
+from PURVIMUSIC import app
+from pyrogram import filters
+
+
+@app.on_message(filters.command("fake"))
+async def address(_, message):
+    query = message.text.split(maxsplit=1)[1].strip()
+    url = f"https://randomuser.me/api/?nat={query}"
+    response = requests.get(url)
+    data = response.json()
+
+    if "results" in data:
+        user_data = data["results"][0]
+
+        
+        name = f"{user_data['name']['title']} {user_data['name']['first']} {user_data['name']['last']}"
+        address = f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}" 
+        city = user_data['location']['city']
+        state = user_data['location']['state']
+        country = user_data['location']['country'] 
+        postal = user_data['location']['postcode']
+        email = user_data['email']
+        phone = user_data['phone']
+        picture_url = user_data['picture']['large']
+
+        
+        caption = f"""
+﹝⌬﹞**ɴᴀᴍᴇ** ⇢ {name}
+﹝⌬﹞**ᴀᴅᴅʀᴇss** ⇢ {address}
+﹝⌬﹞**ᴄᴏᴜɴᴛʀʏ** ⇢ {country}
+﹝⌬﹞**ᴄɪᴛʏ** ⇢ {city}
+﹝⌬﹞**sᴛᴀᴛᴇ** ⇢ {state}
+﹝⌬﹞**ᴘᴏsᴛᴀʟ** ⇢ {postal}
+﹝⌬﹞**ᴇᴍᴀɪʟ** ⇢ {email}
+﹝⌬﹞**ᴘʜᴏɴᴇ** ⇢ {phone}
+
+        """
+
+        
+        await message.reply_photo(photo=picture_url, caption=caption)
+    else:
+        await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.")
---
+++ PURVIMUSIC/plugins/yumi/write.py
@@ -0,0 +1,55 @@
+from pyrogram import filters
+from pyrogram import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import  BOT_USERNAME
+from datetime import datetime
+from PURVIMUSIC import app as app
+import requests
+
+@app.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+        text =message.text.split(None, 1)[1]
+    m =await message.reply_text( "Please wait...,\n\nWriting your text...")
+    write = requests.get(f"https://apis.xditya.me/write?text={text}").url
+
+    caption = f"""
+sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+✨ ᴡʀɪᴛᴛᴇɴ ʙʏ : [𝐘ᴜᴍɪᴋᴏᴏ](https://t.me/{BOT_USERNAME})
+🥀 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}
+"""
+    await m.delete()
+    await message.reply_photo(photo=write,caption=caption)
+
+mod_name = "WʀɪᴛᴇTᴏᴏʟ"
+
+help = """
+
+ ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡʜɪᴛᴇ ᴘᴀɢᴇ ᴡɪᴛʜ ᴀ ᴘᴇɴ 🖊
+
+❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
+ """
+
+
+#----------
+
+@app.on_message(filters.command("day"))
+def date_to_day_command(client: Client, message: Message):
+    try:
+        # Extract the date from the command message......
+        command_parts = message.text.split(" ", 1)
+        if len(command_parts) == 2:
+            input_date = command_parts[1].strip()
+            date_object = datetime.strptime(input_date, "%Y-%m-%d")
+            day_of_week = date_object.strftime("%A")
+
+            # Reply with the day of the week
+            message.reply_text(f"The day of the week for {input_date} is {day_of_week}.")
+
+        else:
+            message.reply_text("Please provide a valid date in the format `/day 1947-08-15` ")
+
+    except ValueError as e:
+        message.reply_text(f"Error: {str(e)}")
