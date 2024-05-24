16400ebd475f0cbb443bdc18ab6d42aaaca1b958
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-25 02:48:06 +0530

    Update mustjoin.py

--- PURVIMUSIC/plugins/bot/mustjoin.py
+++ PURVIMUSIC/plugins/bot/mustjoin.py
@@ -17,7 +17,7 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://" + MUST_JOIN
+                link = "https://t.me/" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
