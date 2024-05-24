89360fe0b04c8dc436a01601d4650d7f26a34893
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-25 00:28:28 +0530

    Add files via upload

---
+++ PURVIMUSIC/plugins/admins/stop.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from PURVIMUSIC import app
+from PURVIMUSIC.core.call import PURVI
+from PURVIMUSIC.utils.database import set_loop
+from PURVIMUSIC.utils.decorators import AdminRightsCheck
+from PURVIMUSIC.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await PURVI.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
