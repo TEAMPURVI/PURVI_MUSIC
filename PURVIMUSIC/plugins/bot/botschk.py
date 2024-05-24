c7cd84e591a5988d9e4d482359fae5126bf933c7
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-25 00:25:59 +0530

    Add files via upload

---
+++ PURVIMUSIC/plugins/bot/botschk.py
@@ -0,0 +1,51 @@
+import random
+from pyrogram import filters
+from PURVIMUSIC import app
+from PURVIMUSIC import *
+from ... import *
+import config
+
+from ...logging import LOGGER
+
+from PURVIMUSIC import app, userbot
+from PURVIMUSIC.core.userbot import *
+
+import asyncio
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import OWNER_ID
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from dotenv import load_dotenv
+from DAXXMUSIC.core.userbot import Userbot
+from datetime import datetime
+
+# Assuming Userbot is defined elsewhere
+userbot = Userbot()
+
+
+BOT_LIST = ["purvi_chat_bot", "king_string_session_bot", "king_x_musicbot", "king_xuser_bot", "king_banall_bot"]
+
+@app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
+async def bots_chk(_, message):
+    msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
+    response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
+    for bot_username in BOT_LIST:
+        try:
+            bot = await app.get_users(bot_username)
+            bot_id = bot.id
+            await asyncio.sleep(0.5)
+            bot_info = await app.send_message(bot_id, "/start")
+            await asyncio.sleep(3)
+            async for bot_message in app.get_chat_history(bot_id, limit=1):
+                if bot_message.from_user.id == bot_id:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
+                else:
+                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
+        except Exception:
+            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
+    
+    await msg.edit_text(response)
+                
---
+++ PURVIMUSIC/plugins/bot/mustjoin.py
@@ -0,0 +1,39 @@
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from PURVIMUSIC import app
+
+#--------------------------
+
+MUST_JOIN = "𝐑𝐀𝐔𝐒𝐇𝐀𝐍 𝐁𝐀𝐁𝐘"
+#------------------------
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/PURVI_UPDATES" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://graph.org/file/561c8a1c1f0dc6a5a7c23.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("๏Jᴏɪɴ๏", url=link),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
