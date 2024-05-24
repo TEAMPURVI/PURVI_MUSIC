d0a4f79556424810f7973bfc34c36cd3ab228cdd
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-25 00:15:48 +0530

    Add files via upload

---
+++ PURVIMUSIC/plugins/tools/imposter.py
@@ -0,0 +1,139 @@
+import random 
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from PURVIMUSIC.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
+from PURVIMUSIC import app
+
+MISHI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
+
+
+ROY = [
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ",
+            url=f"https://t.me/Purvi_music_Bot?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/PURVI_UPDATES")
+    ],
+]
+
+
+@app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=69)
+async def chk_usr(_, message: Message):
+    if message.sender_chat or not await check_pretender(message.chat.id):
+        return
+    if not await usr_data(message.from_user.id):
+        return await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    usernamebefore, first_name, lastname_before = await get_userdata(message.from_user.id)
+    msg = ""
+    if (
+        usernamebefore != message.from_user.username
+        or first_name != message.from_user.first_name
+        or lastname_before != message.from_user.last_name
+    ):
+        msg += f"""
+**♥︎ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ♥︎**
+
+**๏ ɴᴀᴍᴇ** ➛ {message.from_user.mention}
+**๏ ᴜsᴇʀ ɪᴅ** ➛ {message.from_user.id}
+"""
+    if usernamebefore != message.from_user.username:
+        usernamebefore = f"@{usernamebefore}" if usernamebefore else "NO USERNAME"
+        usernameafter = (
+            f"@{message.from_user.username}"
+            if message.from_user.username
+            else "NO USERNAME"
+        )
+        msg += """
+**♥︎ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
+""".format(bef=usernamebefore, aft=usernameafter)
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    if first_name != message.from_user.first_name:
+        msg += """
+**♥︎ ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
+""".format(
+            bef=first_name, aft=message.from_user.first_name
+        )
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    if lastname_before != message.from_user.last_name:
+        lastname_before = lastname_before or "NO LAST NAME"
+        lastname_after = message.from_user.last_name or "NO LAST NAME"
+        msg += """
+**♥︎ ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
+""".format(
+            bef=lastname_before, aft=lastname_after
+        )
+        await add_userdata(
+            message.from_user.id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
+        )
+    if msg != "":
+        await message.reply_photo(random.choice(MISHI), caption=msg, reply_markup=InlineKeyboardMarkup(ROY),)
+
+
+@app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
+async def set_mataa(_, message: Message):
+    if len(message.command) == 1:
+        return await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")
+    if message.command[1] == "enable":
+        cekset = await impo_on(message.chat.id)
+        if cekset:
+            await message.reply("**ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.**")
+        else:
+            await impo_on(message.chat.id)
+            await message.reply(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ᴇɴᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ғᴏʀ** {message.chat.title}")
+    elif message.command[1] == "disable":
+        cekset = await impo_off(message.chat.id)
+        if not cekset:
+            await message.reply("**ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ.**")
+        else:
+            await impo_off(message.chat.id)
+            await message.reply(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ᴅɪsᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ғᴏʀ** {message.chat.title}")
+    else:
+        await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")
+
+    
---
+++ PURVIMUSIC/plugins/tools/ping.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import *
+from PURVIMUSIC import app
+from PURVIMUSIC.core.call import PURVI
+from PURVIMUSIC.utils import bot_sys_stats
+from PURVIMUSIC.utils.decorators.language import language
+from PURVIMUSIC.utils.inline import supp_markup
+from config import BANNED_USERS
+
+
+@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await PURVI.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
