39215ef1e8d48c579443f8066aa9e39b7ee8a4f7
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-29 01:25:00 +0530

    Update Gpt.py

--- PURVIMUSIC/plugins/tools/Gpt.py
+++ PURVIMUSIC/plugins/tools/Gpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @About_Alpha_Baby",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @About_Alpha_dad",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
